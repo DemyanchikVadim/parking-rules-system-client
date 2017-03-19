57344e8cb989c95e31124d9c294b53dec8df98fe
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-03-19 22:17:14 +0300

    Adding login page component and login form containers

---
+++ src/components/Login/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginForm from '../../containers/Login/LoginForm'
+
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="login-page">
+          <LoginForm />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LoginPage;
