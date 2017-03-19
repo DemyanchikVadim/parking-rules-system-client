20c30cf708f3cbdf3c2a6c92b12e66717a479a3e
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-03-19 22:23:12 +0300

    Adding root reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import auth from './auth';
+import flashMessages from './flashMessages';
+
+export default combineReducers({
+  auth,
+  flashMessages,
+});
