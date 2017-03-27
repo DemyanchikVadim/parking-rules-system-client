289651fa35cc23a1e6352e8226ec191b187351e7
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-03-27 21:43:35 +0300

    Add reports reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import reports from './reports';
 import auth from './auth';
 import flashMessages from './flashMessages';
 
 export default combineReducers({
+  reports,
   auth,
-  flashMessages,
+  flashMessages
 });
