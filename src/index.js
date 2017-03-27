c47012679d0819d99a1fafc6a24114bd7e93044c
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-03-27 21:33:49 +0300

    Change browserr outer to router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import '../public/index.css';
 import jwtDecode from 'jwt-decode';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import setAuthorizationToken from './utils/setAuthorizationToken';
 import { setCurrentUser } from './actions/authActions';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
@@ -25,11 +24,11 @@ if (localStorage.jwtToken) {
 }
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Router>
   ,
   document.getElementById('root')
 );
